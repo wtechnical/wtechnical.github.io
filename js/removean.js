9c5268f6a3e03816f4b08c53ab161c828fd20568
wtechnical <60771318+wtechnical@users.noreply.github.com>
2020-02-10 14:18:13 +0500

    Add files via upload

---
+++ js/removean.js
@@ -0,0 +1,35 @@
+		var winWid = window.innerWidth;
+			if (winWid <= 992) {
+				$('.animated').removeClass('animated');
+				$('.animatedParent').removeClass('animatedParent');
+				$('.growIn').removeClass('growIn');
+			}
+		
+		var winWid = window.innerWidth;
+			if (winWid <= 992) {
+				$('.animated').removeClass('animated');
+				$('.animatedParent').removeClass('animatedParent');
+				$('.rotateInDownLeft').removeClass('rotateInDownLeft');
+			}
+		
+		var winWid = window.innerWidth;
+			if (winWid <= 992) {
+				$('.animated').removeClass('animated');
+				$('.animatedParent').removeClass('animatedParent');
+				$('.rotateInDownRight').removeClass('rotateInDownRight');
+			}
+		var winWid = window.innerWidth;
+			if (winWid <= 992) {
+				$('.animated').removeClass('animated');
+				$('.animatedParent').removeClass('animatedParent');
+				$('.bounceInUp').removeClass('bounceInUp');
+			}
+		
+		var winWid = window.innerWidth;
+			if (winWid <= 992) {
+				$('.animated').removeClass('animated');
+				$('.animatedParent').removeClass('animatedParent');
+				$('.bounceInDown').removeClass('bounceInDown');
+			}
+		$('.footer').addClass('animatedParent')
+		$('.footer').addClass('animated')
